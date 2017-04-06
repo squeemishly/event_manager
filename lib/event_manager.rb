d22e4728e6d6670ca373faf7dc63495387f8f613
squeemishly <squeemishly@gmail.com>
2017-04-06 15:15:06 -0600

    ERB

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,25 +1,39 @@
 require 'csv'
+require 'sunlight/congress'
+require 'erb'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
-  if zipcode == nil
-    zipcode = "00000"
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust 5, "0"
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
   end
+
+  legislator_string = legislator_names.join(", ")
 end
 
 puts "Event Manager Initialized!"
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  puts form_letter
 
 end
