693026fdfad9056a44a3f847a50604ba6a77db38
squeemishly <squeemishly@gmail.com>
2017-04-06 14:12:02 -0600

    changes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,25 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode == nil
+    zipcode = "00000"
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust 5, "0"
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 puts "Event Manager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  zip_code = row[:zipcode]
-  puts "#{name} #{zip_code}"
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
+
 end
